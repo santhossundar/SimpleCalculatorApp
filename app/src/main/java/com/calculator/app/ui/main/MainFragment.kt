d7bcfdc7c162a10bbb5caac34ebe27a0ea3b3fbb
santhossundar <santhoshsunthar@gmail.com>
2023-07-16 17:39:31 +0530

    Added initial project files

---
+++ app/src/main/java/com/calculator/app/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.calculator.app.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.calculator.app.R
+
+class MainFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        var view = inflater.inflate(R.layout.fragment_main, container, false)
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/calculator/app/ui/main/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.calculator.app.ui.main
+
+import androidx.core.text.isDigitsOnly
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel() : ViewModel() {
+
+}
