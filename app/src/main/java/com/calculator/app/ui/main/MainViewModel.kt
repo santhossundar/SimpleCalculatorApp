6704128e5069879ae318815a67e8ac3537f0452c
santhossundar <santhoshsunthar@gmail.com>
2023-07-17 09:43:51 +0530

    Implemented user input fields validation function of #MainViewModel

--- app/src/main/java/com/calculator/app/ui/main/MainViewModel.kt
+++ app/src/main/java/com/calculator/app/ui/main/MainViewModel.kt
@@ -1,10 +1,28 @@
 package com.calculator.app.ui.main
 
+import androidx.core.text.isDigitsOnly
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel() : ViewModel() {
     var result = MutableLiveData<Double>(0.0)
+
+    private val _userInputFieldError = MutableLiveData<String?>()
+    val userInputFieldError: LiveData<String?> = _userInputFieldError
+
+    fun validateUserInputFields(firstValue: String, secondValue: String): Boolean {
+        if (firstValue.isBlank() || secondValue.isBlank()) {
+            _userInputFieldError.value = "Input fields cannot be blank!"
+            return false
+        } else if (!firstValue.isDigitsOnly() || !secondValue.isDigitsOnly()) {
+            _userInputFieldError.value = "Only numeric characters can be allowed!"
+            return false
+        } else {
+            _userInputFieldError.value = null
+            return true
+        }
+    }
 
     fun add(valueOne: String, valueTwo: String) {
         val firstValue = valueOne.toDouble()
